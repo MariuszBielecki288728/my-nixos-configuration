0917451677ad1592880ab0284e7f6e8071bae9a4
Mariusz Bielecki <22575146+MariuszBielecki288728@users.noreply.github.com>
2026-08-02 02:15:15 +0200

    upgrade Actual Discord bot to v0.6.0

--- modules/application.nix
+++ modules/application.nix
@@ -59,11 +59,11 @@ let
     };
     discordBot = {
       imageName = "ghcr.io/mariuszbielecki288728/actual-discord-bot";
-      sourceDigest = "sha256:349b8d9ed77d1be0d28365562ce1b9f4469a4f5edf578924276b57a5b89d4bfb";
-      contentDigest = "sha256:349b8d9ed77d1be0d28365562ce1b9f4469a4f5edf578924276b57a5b89d4bfb";
-      nixHash = "sha256-vkPtgUVkJ0QSL43srVpnnLFAQh/+iDGAxw0nR7gm0vU=";
-      # OCI revision: 8d987b24371e87de698581916d46ac2d45ad42d4.
-      version = "v0.5.0";
+      sourceDigest = "sha256:90119cec54caeef16b3756d5b67b1d632cc5f97812232a75b5e1ce2df7568568";
+      contentDigest = "sha256:90119cec54caeef16b3756d5b67b1d632cc5f97812232a75b5e1ce2df7568568";
+      nixHash = "sha256-b6YM83gfmOPKlzfd1k/DpNIr16E03CjoZJG8S1FQfno=";
+      # OCI revision: 02e05548429c1961e59a6b451d41dcb90fdfb345.
+      version = "v0.6.0";
     };
   };
 
